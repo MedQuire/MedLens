5fad1ff06c73fa51954650ec4ceabe5b6c8e1cf3
Momoh Olasunkanmi <momoholasunkanmi077@gmail.com>
2026-09-19 11:54:32 +0100

    c

--- app/declarations.d.ts
+++ app/declarations.d.ts
@@ -1,6 +1,12 @@
+/// <reference types="expo/types" />
+
 declare module '@react-navigation/native';
 declare module '@react-navigation/native-stack';
 declare module '@react-navigation/bottom-tabs';
+
+declare module 'expo-web-browser';
+declare module 'expo-linking';
+declare module 'expo-auth-session';
 
 declare namespace NodeJS {
   interface ProcessEnv {
